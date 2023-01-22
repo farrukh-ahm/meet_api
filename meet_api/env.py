fc8fce7d826463ea49e8992f3961c43d63ec3908
farrukh-ahm <farrukh.adnan@yahoo.co.in>
2023-01-22 17:06:25 +0000

    We install gunicorn and change settings file for deployment

--- meet_api/env.py
+++ meet_api/env.py
@@ -1,5 +1,5 @@
 import os
 
-os.environ['SECRET_KEY'] = "SecretAPICodes"
-# os.environ['DEV'] = '1'
+os.environ.setdefault("SECRET_KEY", "SecretAPICodes")
+os.environ['DEV'] = '1'
 os.environ.setdefault("DATABASE_URL", "postgres://xclqqqru:lazCWfSl8KvjFy-EJ8zeEhmpiBsCfMq1@kandula.db.elephantsql.com/xclqqqru")
--- meet_api/meet_api/settings.py
+++ meet_api/meet_api/settings.py
@@ -31,10 +31,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', cast=bool)
-# DEBUG = True
+# DEBUG = config('DEBUG', cast=bool)
+DEBUG = 'DEV' in os.environ
 
-ALLOWED_HOSTS = []
+
+ALLOWED_HOSTS = ['localhost', 'letsmeet-api.herokuapp.com']
 
 
 # Application definition
@@ -104,6 +105,7 @@ SIMPLE_JWT = {
 
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -141,6 +143,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:8000",
 ]
 
+CORS_ALLOW_CREDENTIALS = True
 
 
 # Database
