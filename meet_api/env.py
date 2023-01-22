f11b4dca79c3bcfae4765bfc974a082b64590004
farrukh-ahm <farrukh.adnan@yahoo.co.in>
2023-01-22 12:45:11 +0000

    We reinstall missing packages

---
+++ meet_api/env.py
@@ -0,0 +1,4 @@
+import os
+
+os.environ['SECRET_KEY'] = "SecretAPICodes"
+# os.environ['DEBUG'] = True
--- meet_api/meet_api/settings.py
+++ meet_api/meet_api/settings.py
@@ -14,6 +14,9 @@ from datetime import timedelta
 from pathlib import Path
 
 from decouple import config
+import os
+if os.path.exists('env.py'):
+    import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,10 +26,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+# SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
+# DEBUG = True
 
 ALLOWED_HOSTS = []
 
