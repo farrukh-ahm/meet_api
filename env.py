53b1f38dda7c94b42aa0b071577effeb0cf2aa71
farrukh-ahm <farrukh.adnan@yahoo.co.in>
2023-01-28 22:18:31 +0000

    did some changes to env file

--- env.py
+++
@@ -1,6 +0,0 @@
-import os
-
-os.environ.setdefault("SECRET_KEY", "SecretAPICodes")
-os.environ['DEV'] = '1'
-os.environ.setdefault("DATABASE_URL", "postgres://xclqqqru:lazCWfSl8KvjFy-EJ8zeEhmpiBsCfMq1@kandula.db.elephantsql.com/xclqqqru")
-os.environ['CLOUDINARY_URL'] = "cloudinary://265487814213721:owt3PnJbXJMuba6ff8x3MEjigBc@farrukh-ahm"
