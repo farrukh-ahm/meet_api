6dea9d90dbe1295f2edf415659c1b80b4723b46a
farrukh-ahm <farrukh.adnan@yahoo.co.in>
2023-01-28 15:09:47 +0000

    Add front-end links to cors_allowed for connection

--- meet_api/settings.py
+++ meet_api/settings.py
@@ -140,8 +140,8 @@ CORS_ALLOWED_ORIGINS = [
     "https://example.com",
     "https://sub.example.com",
     "http://localhost:3000",
-    "https://3000-farrukhahm-letsmeet-7dc4gmcslzx.ws-eu84.gitpod.io/",
-    "https://lets-meet.herokuapp.com/",
+    "https://3000-farrukhahm-letsmeet-7dc4gmcslzx.ws-eu84.gitpod.io",
+    "https://lets-meet.herokuapp.com",
 ]
 
 CORS_ALLOW_CREDENTIALS = True
