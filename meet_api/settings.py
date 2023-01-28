65bad44bd71314ab70783b5aae07657514ccce5c
farrukh-ahm <farrukh.adnan@yahoo.co.in>
2023-01-28 14:36:37 +0000

    Minor changes

--- meet_api/settings.py
+++ meet_api/settings.py
@@ -56,10 +56,10 @@ REST_FRAMEWORK = {
     ),
 }
 
-# if 'DEV' not in os.environ:
-#     REST_FRAMEWORK['DEFAULT_RENDERER_CLASSES'] = [
-#         'rest_framework.renderers.JSONRenderer',
-#     ]
+if 'DEV' not in os.environ:
+    REST_FRAMEWORK['DEFAULT_RENDERER_CLASSES'] = [
+        'rest_framework.renderers.JSONRenderer',
+    ]
 
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(days=10),
