3df8e3015d3e886fcfc445ba4ef36f45fdc9de10
farrukh-ahm <farrukh.adnan@yahoo.co.in>
2023-02-01 13:36:31 +0000

    Change account url for profile pic

--- account/urls.py
+++ account/urls.py
@@ -4,7 +4,7 @@ from django.urls import path
 urlpatterns = [
     path('', views.get_profile, name="profile"),
     path('<str:username>', views.get_profile_username, name="profile-username"),
-    path('profilepicupload', views.uploadProfileImage, name="profile-pic-upload"),
+    path('profileupload/', views.uploadProfileImage, name="profile-pic-upload"),
     path('admin/all/', views.get_allprofile, name="all-profile"),
     path('update-profile/', views.update_profile, name="update-profile"),
 ]
